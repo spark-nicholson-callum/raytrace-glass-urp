c88a64276c3f7cf4380de5d52f39c4f0dc029374
Callum Nicholson <nicholson.callum@spark-creative.co.jp>
2026-07-06 14:09:56 +0900

    Added an output projection shader

--- Runtime/Scripts/HybridLensRendererFeature.cs
+++ Runtime/Scripts/HybridLensRendererFeature.cs
@@ -8,6 +8,7 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
     public class HybridLensData : ContextItem
     {
         public TextureHandle NormalBufferHandle;
+        public TextureHandle OutputTextureHandle;
         public BufferHandle ActivePixelsBufferHandle;
 
         public override void Reset()
@@ -29,6 +30,7 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
 
         private class PassData
         {
+            // TODO // Normal buffer doesn't have to be in pass data
             public TextureHandle NormalBuffer;
             public RendererListHandle RendererList;
         }
@@ -71,7 +73,7 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
                     context.cmd.DrawRendererList(data.RendererList);
                 });
             }
-    }
+        }
     }
 
     public class CompactionPass : ScriptableRenderPass
@@ -188,6 +190,8 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
 
             TextureHandle outputTextureHandle = UniversalRenderer.CreateRenderGraphTexture(renderGraph, texDesc, "_RayTraceOutput", false);
 
+            lensData.OutputTextureHandle = outputTextureHandle;
+
             BufferDesc argsDesc = new BufferDesc(4, sizeof(uint))
             {
                 name = "Indirect Args Buffer",
@@ -242,10 +246,65 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
         }
     }
 
+    private class ProjectorPass : ScriptableRenderPass
+    {
+        private static readonly int rayTraceOutputId = Shader.PropertyToID("_RayTraceOutput");
+        private ShaderTagId shaderTagId = new ShaderTagId("HybridLens/Project");
+        private FilteringSettings filteringSettings;
+
+        public ProjectorPass()
+        {
+            filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        }
+
+        private class PassData
+        {
+            public TextureHandle OutputTexture;
+            public RendererListHandle RendererList;
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            var lensData      = frameData.Get<HybridLensData>();
+            var renderingData = frameData.Get<UniversalRenderingData>();
+            var cameraData    = frameData.Get<UniversalCameraData>();
+            var lightData     = frameData.Get<UniversalLightData>();
+            var resourceData  = frameData.Get<UniversalResourceData>();
+
+            var sortingCriteria = cameraData.defaultOpaqueSortFlags;
+            var drawingSettings = CreateDrawingSettings(shaderTagId, renderingData, cameraData, lightData, sortingCriteria);
+
+            var listParams = new RendererListParams(renderingData.cullResults, drawingSettings, filteringSettings);
+            var rendererListHandle = renderGraph.CreateRendererList(listParams);
+
+            using (var builder = renderGraph.AddRasterRenderPass<PassData>("Lens Projection Pass", out var passData))
+            {
+                // Needed to enable setting the output texture
+                builder.AllowGlobalStateModification(true);
+
+                builder.UseRendererList(rendererListHandle);
+                passData.RendererList = rendererListHandle;
+
+                builder.UseTexture(lensData.OutputTextureHandle, AccessFlags.Read);
+                passData.OutputTexture = lensData.OutputTextureHandle;
+
+                builder.SetRenderAttachment(resourceData.activeColorTexture, 0, AccessFlags.Write);
+                builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture, AccessFlags.Read);
+
+                builder.SetRenderFunc(static (PassData data, RasterGraphContext context) =>
+                {
+                    context.cmd.SetGlobalTexture(rayTraceOutputId, data.OutputTexture);
+                    context.cmd.DrawRendererList(data.RendererList);
+                });
+            }
+        }
+    }
+
     [SerializeField] private ComputeShader lensComputeShader;
     private GatherPass gatherPass;
     private CompactionPass compactionPass;
     private TracePass tracePass;
+    private ProjectorPass projectorPass;
 
     public override void Create()
     {
@@ -259,6 +318,9 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
 
         tracePass = new TracePass(lensComputeShader);
         tracePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques + 2;
+
+        projectorPass = new ProjectorPass();
+        projectorPass.renderPassEvent = RenderPassEvent.AfterRenderingSkybox;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
@@ -277,5 +339,6 @@ public class HybridLensRendererFeature : ScriptableRendererFeature
         renderer.EnqueuePass(gatherPass);
         renderer.EnqueuePass(compactionPass);
         renderer.EnqueuePass(tracePass);
+        renderer.EnqueuePass(projectorPass);
     }
 }
--- Runtime/Shaders/HybridLens.shader
+++ Runtime/Shaders/HybridLens.shader
@@ -47,6 +47,52 @@ Shader "Custom/HybridLens"
 
             ENDHLSL
         }
+
+        Pass
+        {
+            Name "LensProjector"
+            Tags {"LightMode" = "HybridLens/Project"}
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+            struct Attributes
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct Varyings
+            {
+                float4 pos : SV_POSITION;
+                float4 screenPos : TEXCOORD0;
+            };
+
+            TEXTURE2D(_RayTraceOutput);
+            SAMPLER(sampler_RayTraceOutput);
+
+            Varyings vert(Attributes IN)
+            {
+                Varyings OUT;
+
+                OUT.pos = TransformObjectToHClip(IN.vertex.xyz);
+                OUT.screenPos = ComputeScreenPos(OUT.pos);
+
+                return OUT;
+            }
+
+            float4 frag(Varyings IN) : SV_Target
+            {
+                float2 screenUv = IN.screenPos.xy / IN.screenPos.w;
+                float4 rtColor = SAMPLE_TEXTURE2D(_RayTraceOutput, sampler_RayTraceOutput, screenUv);
+
+                clip(rtColor.a - 0.01);
+                return rtColor;
+            }
+
+            ENDHLSL
+        }
     }
 }
 
