d62a37a2459d9ac9aa2be204bf274f309f740dcb
Callum Nicholson <nicholson.callum@spark-creative.co.jp>
2026-07-10 16:34:35 +0900

    Added texture management as well

--- Runtime/Scripts/RayTracingSceneManager.cs
+++ Runtime/Scripts/RayTracingSceneManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -9,6 +10,16 @@ namespace CallumNicholson.RaytraceGlassURP
         public static RayTracingSceneManager Instance;
 
         public RayTracingAccelerationStructure Rtas { get; private set; }
+        public RenderTexture GlobalTextureArray { get; private set; }
+        public ComputeBuffer GlobalTextureIndexBuffer { get; private set; }
+
+        [SerializeField] private int MaxTextures = 64;
+        [SerializeField] private int TextureResolution = 512;
+
+        private Texture2D fallbackTexture;
+
+        private Dictionary<Texture, int> textureSliceMap = new();
+        private int currentSlice;
 
         public void Awake()
         {
@@ -20,6 +31,22 @@ namespace CallumNicholson.RaytraceGlassURP
             settings.rayTracingModeMask = RayTracingAccelerationStructure.RayTracingModeMask.DynamicTransform
                                         | RayTracingAccelerationStructure.RayTracingModeMask.Static;
             Rtas = new RayTracingAccelerationStructure(settings);
+
+            GlobalTextureArray = new RenderTexture(TextureResolution, TextureResolution, 0, RenderTextureFormat.ARGB32)
+            {
+                dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray,
+                volumeDepth = MaxTextures,
+                useMipMap = true,
+                autoGenerateMips = false,
+                enableRandomWrite = true
+            };
+            GlobalTextureArray.Create();
+
+            fallbackTexture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+            fallbackTexture.SetPixel(0, 0, Color.white);
+            fallbackTexture.Apply();
+
+            GetTextureSlice(fallbackTexture);
         }
 
         public void RebuildSceneData()
@@ -30,6 +57,9 @@ namespace CallumNicholson.RaytraceGlassURP
                 .Where(x => x.gameObject.activeInHierarchy)
                 .ToArray();
 
+            var sliceData = new int[allRenderers.Length];
+            int currentIndex = 0;
+
             foreach (var renderer in allRenderers)
             {
                 int subMeshCount = renderer.sharedMaterials.Length;
@@ -38,13 +68,45 @@ namespace CallumNicholson.RaytraceGlassURP
                     .ToArray();
 
                 Rtas.AddInstance(renderer, subMeshFlags);
+
+                Texture mainTexture = null;
+                if (renderer.sharedMaterial != null && renderer.sharedMaterial.HasProperty("_MainTex"))
+                    mainTexture = renderer.sharedMaterial.mainTexture;
+                sliceData[currentIndex] = GetTextureSlice(mainTexture);
+
+                ++currentIndex;
             }
+
+            if (GlobalTextureIndexBuffer != null) GlobalTextureIndexBuffer.Release();
+            GlobalTextureIndexBuffer = new ComputeBuffer(Mathf.Max(1, allRenderers.Length), 4);
+            GlobalTextureIndexBuffer.SetData(sliceData);
+        }
+
+        public int GetTextureSlice(Texture source)
+        {
+            if (source == null) return 0;
+
+            if (textureSliceMap.TryGetValue(source, out int existingSlice)) return existingSlice;
+            if (currentSlice >= MaxTextures)
+            {
+                Debug.LogWarning("Ray tracing texture atlas is full!");
+                return 0;
+            }
+
+            Graphics.Blit(source, GlobalTextureArray, 0, currentSlice);
+            GlobalTextureArray.GenerateMips();
+            textureSliceMap[source] = currentSlice;
+
+            return currentSlice++;
         }
 
         public void OnDestroy()
         {
             if (Instance == this) Instance = null;
             if (Rtas != null) Rtas.Release();
+            if (GlobalTextureArray != null) GlobalTextureArray.Release();
+            if (GlobalTextureIndexBuffer != null) GlobalTextureIndexBuffer.Release();
+            if (fallbackTexture != null) Destroy(fallbackTexture);
         }
     }
 }
